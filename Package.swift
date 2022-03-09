22d799cc8f5f0e8223844620e6fa4922466caa49
Zach Fuller <zfuller@grubhub.com>
2022-03-09 17:01:09 -0600

    Updated Package.swift to fix error when gathering code coverage

--- Package.swift
+++ Package.swift
@@ -25,13 +25,24 @@
 
 import PackageDescription
 
-let package = Package(name: "AFNetworking",
-                      platforms: [.macOS(.v10_10),
-                                  .iOS(.v9),
-                                  .tvOS(.v9),
-                                  .watchOS(.v2)],
-                      products: [.library(name: "AFNetworking",
-                                          targets: ["AFNetworking"])],
-                      targets: [.target(name: "AFNetworking",
-                                        path: "AFNetworking",
-                                        publicHeadersPath: "")])
+let package = Package(
+    name: "AFNetworking",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)],
+    products: [
+        .library(name: "AFNetworking", targets: ["AFNetworking"])
+    ],
+    targets: [
+        .target(
+            name: "AFNetworking",
+            path: "AFNetworking",
+            publicHeadersPath: "",
+            linkerSettings: [
+                .unsafeFlags(["-fprofile-instr-generate"])
+            ]
+        )
+    ]
+)
